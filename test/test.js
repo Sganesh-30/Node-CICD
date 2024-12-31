d0ce40028bb651453b1a7e5516f46d8c6444952a
Ganesh <ganeshsg430@gmail.com>
2024-12-31 18:30:59 +0530

    modified test.js

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 
 describe('Sample Test', function () {
-    test('should return true', function () {
+    it('should return true', function () {
         assert.strictEqual(true, true);
     });
 });
